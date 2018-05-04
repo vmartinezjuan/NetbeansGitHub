8e639bee7ff2d811e6151d18861099650fed4529
Administrador <Administrador@LaptopDell>
2018-05-04 22:44:39 +0200

    Rama Rectangulo: añadida al index y commit

---
+++ src/gitproyec/Rectangulo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gitproyec;
+
+/* * Rectangulo.java  * Creado el  04-may-2018, 22:40:48  
+ * @autor Vicente Martínez Juan * Contacta conmigo en torrent636588336@gmail.com*/
+
+public class Rectangulo {
+    float alto, ancho;
+    public Rectangulo(float l1, float l2){
+        alto=11;
+        ancho=12;
+    }
+}
