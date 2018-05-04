354bb89c54f24dbc439e8e7f256fcc258d2d3c43
Administrador <Administrador@LaptopDell>
2018-05-04 20:19:24 +0200

    Vers. 1.0

---
+++ src/gitproyec/Circulo.java
@@ -0,0 +1,48 @@
+//Created dir: G:\DAM\Programacion\documentacion1\dist\javadoc
+
+package gitproyec;
+/**
+ * @author Vicente Martinez Juan
+ * @version circulo 26-04/2018
+ */
+public class Circulo {
+
+    private double centroX;
+    private double centroY;
+    private double radio;
+
+ /**
+ * Constructor
+ * @param x centro: coordenada X.
+ * @param y centro: coordenada Y.
+ * @param r radio. * 
+ */
+    public Circulo(double x, double y, double r) {
+        centroX = x;
+        centroY = y;
+        radio = r;
+    }
+/**
+* Getter.
+* @return centro: coordenada X.
+*/
+    public double getCentroX() {
+        return centroX;
+    }
+/**
+* Método que Calcula la longitud de la circunferencia (perímetro del círculo).
+* @return perímetro.
+*/
+    public double getPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+/**
+* Método que desplaza el círculo a otro lugar.
+* @param despX movimiento en el eje X.
+* @param despY movimiento en el eje Y.
+*/
+    public void mueve(double despX, double despY) {
+        centroX = centroX + despX;
+        centroY = centroY + despY;
+    }
+}
